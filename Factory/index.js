9ecdbddd8f07de05525dc6410a8ddfc1ac068b04
Joshua Piper <joshuapiper@joshuas-air.lan>
2022-02-13 12:05:30 +1100

    Add factory pattern

--- Factory/index.js
+++ Factory/index.js
@@ -1,3 +1,58 @@
 // Creational Design Pattern
 
-console.log('Hello');
+// A factory manufactures objects.
+// Central location for creating objects.
+// Reduce the need for using the new () keyword.
+
+// A software company may have different classes for their employees.
+// They may have a software tester or a developer. A factory could
+// abstract the creation process of this.
+
+class Developer {
+    constructor(name) {
+        this.name = name;
+        this.type = 'Developer';
+    }
+}
+
+class Tester {
+    constructor(name) {
+        this.name = name;
+        this.type = 'Tester';
+    }
+}
+
+class EmployeeFactory {
+    constructor() {
+        this.employees = [];
+    }
+
+    create(name, type) {
+        switch (type) {
+            case 'Developer':
+                return new Developer(name);
+            case 'Tester':
+                return new Tester(name);
+            default:
+                console.log('Invalid employee type');
+                break;
+        }
+    }
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+}
+
+function say() {
+    console.log(`Hi, I am ${this.name} and I am a ${this.type}`);
+}
+const employeeFactory = new EmployeeFactory();
+
+const employee = employeeFactory.create('John', 'Developer');
+console.log('employee', employee);
+employeeFactory.addEmployee(employee);
+employeeFactory.addEmployee(employeeFactory.create('Jane', 'Tester'));
+
+employeeFactory.employees.forEach(employee => say.call(employee));
+console.log('Fin');
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # JavaScript Design Patterns
+
+## Structural
+Deals with the relationships between other objects.
+
+### Adapter
+
+### Facade
+
+### Bridge
+
+## Creational
+Object creation mechanism.
+Creating many different types of objects at
+a bulk level.
+
+### Singleton
+One instance of a class acting a global centralised store.
+### Factory
+Create objects ...
+
+### Abstract Factory
+Produce a class without specifying its
+specific class name.
+### Bridge
+
+## Behavioural
+Algorithms and assignment of responsibility between objects.
+### Strategy
+
+### Observer
+
+### Iterator
+
+### Command
+
+### Mediator
+
+### Observer
+
+### Visitor
